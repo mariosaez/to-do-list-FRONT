134f17dc65247b8033dd5171205c76fd902d2640
mariosaez <mariosaezvilluendas@gmail.com>
2024-06-23 18:36:50 +0200

    addHome

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,11 @@
-export const Home = () => {}
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+        </div>
+    );
+}
+
+export default Home;
