e3265a1431bb2ac92db49c696c1040db7f115998
mario.saez <mario.saez@movicoders.com>
2024-07-02 09:14:49 +0200

    refactor useDrag

--- src/components/DraggableComponent.tsx
+++ src/components/DraggableComponent.tsx
@@ -2,24 +2,21 @@ import { useDrag } from "react-dnd";
 
 export const DraggableComponent = (props: {id: string, type: string, children: any}) => {
     const {id, type, children} = props;
-    const [{ isDragging }, drag] = useDrag(() => ({
+    const [, drag] = useDrag(() => ({
         type: type,
         item: { id },
-        collect: (monitor) => ({
-          isDragging: !!monitor.isDragging(),
-        }),
+        collect: () => ({})
       }));
 
       return (
         <div
           ref={drag}
           style={{
-            opacity: isDragging ? 0.5 : 1,
             fontWeight: 'bold',
             cursor: 'move',
           }}
         >
           {children}
         </div>
-      );  
-}
+      ); 
+};
